dd9bd8adb6f151054fbfe25a01e5a9a732eaf1da
Manitra Andriamitondra <m.andriamitondra@criteo.com>
2015-05-21 12:32:23 +0700

    Added the projects files

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MSBuild.Front")]
+[assembly: AssemblyDescription("A simple stupid way to share front end assets between msbuild projects")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("MSBuild.Front")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("637cb3e4-2601-47e8-aa2a-31f8a3196f2d")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
