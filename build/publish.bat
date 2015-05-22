0c4eec842c6e030f5b1ce8857d88566c0560be11
Manitra Andriamitondra <m.andriamitondra@criteo.com>
2015-05-22 16:31:47 +0700

    Added tagging to the publish.bat script

--- build/publish.bat
+++ build/publish.bat
@@ -3,9 +3,29 @@ cd %~dp0\..\src
 
 set msbuild=C:\Windows\Microsoft.Net\Framework64\v4.0.30319\MSBuild.exe
 set nuget=nuget
-set version=0.6.1.0
+set git=git
+set version=0.6.1.2
 set config=Release
 
+echo Building the package ...
 %msbuild% MSBuild.Front.sln /p:Configuration=%config%
+if ERRORLEVEL 1 goto error
 %nuget% pack MSBuild.Front.Nuget\MSBuild.Front.Nuget.nuspec -Prop Configuration=%config% -Version %version% -OutputDir MSBuild.Front.Nuget\bin
+if ERRORLEVEL 1 goto error
+
+echo Pushing the package ...
+%git% tag -a nuget-%version% -m "Nuget package version %version%"
+if ERRORLEVEL 1 goto error
 %nuget% push MSBuild.Front.Nuget\bin\MSBuild.Front.%version%.nupkg
+if ERRORLEVEL 1 goto error
+%git% push --tags
+if ERRORLEVEL 1 goto error
+
+goto end
+
+:error
+echo An error occured, deleting the tag in case it has been created
+%git% tag -d nuget-%version%
+
+:end
+echo Finished
