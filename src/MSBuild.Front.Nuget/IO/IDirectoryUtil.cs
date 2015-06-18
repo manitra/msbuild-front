34288d2cd217874c0095b1228306d16267abe7ba
Khanh To <suzhiqing197@gmail.com>
2015-06-18 09:57:26 +0700

    Create symlinks for all necessary folders and files
    Change-Id: I0e147094b39fc3d330c2b6ec20618153e70363ea

---
+++ src/MSBuild.Front.Nuget/IO/IDirectoryUtil.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MSBuild.Front.Nuget.IO
+{
+    public interface IDirectoryUtil
+    {
+        /// <summary>
+        /// Copy all subdirectories and files
+        /// </summary>
+        /// <param name="sourceDirName">The full path of the source directory</param>
+        /// <param name="destDirName">The full path of the destination directory</param>
+        /// <param name="copySubDirs">Whether to copy sub directories</param>
+        void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs);
+    }
+}
--- src/MSBuild.Front.Nuget/Tasks/IncludeFrontDependencies.cs
+++ src/MSBuild.Front.Nuget/Tasks/IncludeFrontDependencies.cs
@@ -1,4 +1,6 @@
-﻿namespace MSBuild.Front.Nuget.Tasks
+﻿using System.Linq;
+
+namespace MSBuild.Front.Nuget.Tasks
 {
     using System;
     using System.Collections.Generic;
@@ -17,15 +19,29 @@
     public class IncludeFrontDependencies : Task
     {
         private readonly ILinkUtil _linkUtil;
+        private readonly IDirectoryUtil _directoryUtil;
 
         public IncludeFrontDependencies()
-            : this(new LinkUtil())
+            : this(new LinkUtil(), new DirectoryUtil())
         {
         }
 
-        public IncludeFrontDependencies(ILinkUtil linkUtil)
+        public IncludeFrontDependencies(ILinkUtil linkUtil, IDirectoryUtil directoryUtil)
         {
             _linkUtil = linkUtil;
+            _directoryUtil = directoryUtil;
+        }
+
+        private enum LinkType
+        {
+            Copy = 0,
+            Symlink = 1
+        }
+
+        private class LinkFolder
+        {
+            public string Folder { get; set; }
+            public LinkType Type { get; set; }
         }
 
         /// <summary>
@@ -34,37 +50,67 @@
         [Required]
         public string ProjectPath { get; set; }
 
+        private readonly LinkFolder[] LinkedFolders = new LinkFolder[5]
+        {
+            new LinkFolder() { Folder = "app", Type = LinkType.Symlink },
+            new LinkFolder() { Folder = "build", Type = LinkType.Copy },
+            new LinkFolder() { Folder = "tests\\unit-tests", Type = LinkType.Symlink },
+            new LinkFolder() { Folder = "tests\\e2e-tests", Type = LinkType.Symlink },
+            new LinkFolder() { Folder = "tests\\util", Type = LinkType.Symlink }
+        };
+
         public override bool Execute()
         {
-            var destDir = Path.Combine(Path.GetDirectoryName(ProjectPath), "app");
-            var ignoreFile = Path.Combine(destDir, ".gitignore");
+            var referencedDirs = GetReferencedDirs(ProjectPath).ToList();
 
-            if (!Directory.Exists(destDir)) Directory.CreateDirectory(destDir);
-            File.WriteAllText(ignoreFile, ".gitignore" + Environment.NewLine);
-
-            // Note that we don't handle recursion here because
-            // the nested dependancies have been built already
-            // which means their respective dependancies
-            // will be in their own app folder
-            foreach (var sourceDir in GetSourceDirs(ProjectPath))
+            foreach (var linkFolder in LinkedFolders)
             {
-                // just means that there is no "app" folder in the referenced project
-                // which means it's not a MSBuild.Front-ready project
-                if (!Directory.Exists(sourceDir))
-                {
-                    Log.LogMessage(MessageImportance.Low, "Skipping '{0}' because it doesn't exist", sourceDir);
-                    continue;
-                }
+                var destDir = Path.Combine(Path.GetDirectoryName(ProjectPath), linkFolder.Folder);
+                var ignoreFile = Path.Combine(destDir, ".gitignore");
 
-                foreach (var item in Directory.GetFileSystemEntries(sourceDir, "*", SearchOption.TopDirectoryOnly))
+                if (!Directory.Exists(destDir)) Directory.CreateDirectory(destDir);
+                File.WriteAllText(ignoreFile, ".gitignore" + Environment.NewLine);
+
+                // Note that we don't handle recursion here because
+                // the nested dependancies have been built already
+                // which means their respective dependancies
+                // will be in their own app folder
+                foreach (var referencedDir in referencedDirs)
                 {
-                    var shortName = Path.GetFileName(item);
-                    _linkUtil.CreateLink(
-                        Path.Combine(destDir, shortName), 
-                        item, 
-                        Directory.Exists(item) ? SymbolicLinkType.Directory : SymbolicLinkType.File
-                    );
-                    File.AppendAllText(ignoreFile, shortName + Environment.NewLine);
+                    var sourceDir = Path.Combine(referencedDir, linkFolder.Folder);
+                    // just means that there is no folder in the referenced project
+                    // which means it's not a MSBuild.Front-ready project
+                    if (!Directory.Exists(sourceDir))
+                    {
+                        Log.LogMessage(MessageImportance.Low, "Skipping '{0}' because it doesn't exist", sourceDir);
+                        continue;
+                    }
+
+                    foreach (var item in Directory.GetFileSystemEntries(sourceDir, "*", SearchOption.TopDirectoryOnly))
+                    {
+                        var shortName = Path.GetFileName(item);
+                        var desItem = Path.Combine(destDir, shortName);
+                        if (linkFolder.Type == LinkType.Symlink)
+                        {
+                            _linkUtil.CreateLink(
+                                desItem,
+                                item,
+                                Directory.Exists(item) ? SymbolicLinkType.Directory : SymbolicLinkType.File
+                                );
+                        }
+                        else if (linkFolder.Type == LinkType.Copy)
+                        {
+                            if (Directory.Exists(item))
+                            {
+                                _directoryUtil.DirectoryCopy(item, desItem, true);
+                            }
+                            else
+                            {
+                                File.Copy(item, desItem, true);
+                            }
+                        }
+                        File.AppendAllText(ignoreFile, shortName + Environment.NewLine);
+                    }
                 }
             }
 
@@ -72,11 +118,11 @@
         }
 
         /// <summary>
-        /// Get the list of the absolute path of the "app" folder of every dependancies of the given csproj file
+        /// Get the list of the absolute path of all dependencies of the given csproj file
         /// </summary>
         /// <param name="projectFile">The full path of a csproj file</param>
         /// <returns></returns>
-        private IEnumerable<string> GetSourceDirs(string projectFile)
+        private IEnumerable<string> GetReferencedDirs(string projectFile)
         {
             const string Ns = "http://schemas.microsoft.com/developer/msbuild/2003";
             var projectContent = XDocument.Load(projectFile);
@@ -109,12 +155,11 @@
                     continue;
                 }
 
-                yield return Path.Combine(
+                yield return Path.GetFullPath(
+                    Path.Combine(
                     projectDir,
-                    Path.GetDirectoryName(csprojFile),
-                    "app");
+                    Path.GetDirectoryName(csprojFile)));
             }
         }
-
     }
 }
